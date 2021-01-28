ce03197d40f7800f691a907357d7a77c25178905
wengzhiyu <wengzhiyu@shenzhenpoly.com>
2021-01-28 16:14:12 +0800

    update

--- zhiyu-pay-service/src/test/java/com/zhiyu/pay/MybatisPlusGenerator02.java
+++ zhiyu-pay-service/src/test/java/com/zhiyu/pay/MybatisPlusGenerator02.java
@@ -1,5 +1,6 @@
 package com.zhiyu.pay;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
@@ -9,15 +10,15 @@ import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
 /**
- * 实体生产类
- *
  * @author wengzhiyu
- * @date 2020/11/12
+ * @date 2021/1/22
  */
-public class MybatisPlusGenerator {
-    static final String TABLE_NAME = "b2b_supplier_info";
-    static final String MODULE_NAME = "b2bsupplierinfo";
+public class MybatisPlusGenerator02 {
+
+    static final String TABLE_NAME = "t_org_company";
+    static final String MODULE_NAME = "orgcompany";
     static final String AUTHOR = "wengzhiyu";
+
 
     //修改TABLE_NAME、MODULE_NAME、AUTHOR后运行main方法
     //注意，会覆盖MODULE_NAME下的所有代码
@@ -27,10 +28,10 @@ public class MybatisPlusGenerator {
 
         // 全局配置
         GlobalConfig globalConfig = new GlobalConfig();
-        String projectPath = "D:/polyWeb1117/hiyu-information-info/zhiyu-pay-service"; //项目目录
+        String projectPath = "D:/mybatisPo"; //项目目录
         System.out.println("11"+projectPath);
-        //String projectPath = "D://generator";
-        globalConfig.setOutputDir(projectPath + "/src/main/java");
+        //globalConfig.setOutputDir(projectPath + "/src/main/java");
+        globalConfig.setOutputDir(projectPath );
         globalConfig.setOpen(true);  //生成后打开文件目录
         globalConfig.setFileOverride(true);  //覆盖文件
         globalConfig.setBaseResultMap(true);  //生成BaseResultMap
@@ -38,22 +39,24 @@ public class MybatisPlusGenerator {
         globalConfig.setServiceName("%sService");  //自定义service接口名
         globalConfig.setDateType(DateType.ONLY_DATE);  //使用java.util.Date
         globalConfig.setAuthor(AUTHOR);
+        globalConfig.setIdType(IdType.ASSIGN_UUID);//主键策略
+        globalConfig.setSwagger2(true);
         autoGenerator.setGlobalConfig(globalConfig);
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
         // 改数据库连接
-        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/wzy?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dsc.setUrl("jdbc:mysql://120.79.150.108:3307/db_bop?useUnicode=true&useSSL=false&characterEncoding=utf8");
         dsc.setDriverName("com.mysql.jdbc.Driver");
         // 改账号密码
         dsc.setUsername("root");
-        dsc.setPassword("123456");
+        dsc.setPassword("poly2017");
         autoGenerator.setDataSource(dsc);
 
         // 包配置
         PackageConfig pc = new PackageConfig();
         // 实体包名
-        pc.setParent("com.zhiyu.pay.entity.pojo");
+        pc.setParent("sas");
         pc.setServiceImpl("service");  //在service包下生成Impl
         pc.setXml("mapper");  //在mapper生成xml
         pc.setModuleName(MODULE_NAME);
@@ -69,8 +72,8 @@ public class MybatisPlusGenerator {
         strategy.setEntityLombokModel(true);
         //生成 @RestController 控制器
         strategy.setRestControllerStyle(true);
-        // 改表前缀
-        //strategy.setTablePrefix("td");
+        //生成实体时去掉表前缀
+        strategy.setTablePrefix("t_org");
         //需要包含的表名，允许正则表达式（与exclude二选一配置）
         strategy.setInclude(TABLE_NAME);
         autoGenerator.setStrategy(strategy);
